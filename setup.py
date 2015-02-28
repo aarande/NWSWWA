d11b5d056abd95913d78e26ce33b920f3a06ded5
Aaron Anderson <aaron.anderson74@yahoo.com>
2015-02-28 15:28:35 -0600

    setup.py and initial package commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='NWSWWA',
+    version='0.1',
+    packages=['nwswwa'],
+    url='https://github.com/aarande/NWSWWA',
+    license='MIT',
+    author='Aaron Anderson',
+    author_email='',
+    description='Python library to parse National Weather Service warnings and advisories'
+)
